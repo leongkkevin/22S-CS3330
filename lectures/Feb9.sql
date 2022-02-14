5d88b8de253613444302cd8964cc563805c88528
leongkkevin <kkleong@smu.edu>
2022-02-14 17:44:55 -0600

    Added inserts

--- lectures/Feb9.sql
+++ lectures/Feb9.sql
@@ -40,6 +40,16 @@ CREATE TABLE employee (
     service_department_id INTEGER REFERENCES service_department(id)
 );
 
+INSERT INTO employee (first_name, last_name, service_department_id) VALUES ('Jeff', 'Bezos', 1),
+                                                                           ('Bob', 'Bobberson', 2),
+                                                                           ('Karen', 'MeanLady', 3),
+                                                                           ('Tony', 'Stank', 3),
+                                                                           ('Hawk', 'Eye', 1),
+                                                                           ('Ben', '10', 2),
+                                                                           ('Peter', 'Griffin', 1),
+                                                                           ('Bob', 'Minion', 2),
+                                                                           ('The', 'Hulk', 4);
+
 CREATE TABLE reservation (
     id SERIAL PRIMARY KEY,
     vehicle_id VARCHAR(30) REFERENCES vehicle(vin),
@@ -67,3 +77,21 @@ CREATE TABLE employee_maintenance(
     maintenance_history_id INTEGER REFERENCES maintenance_history(id),
     PRIMARY KEY (employee_id, maintenance_history_id)
 );
+CREATE TABLE transaction (
+    id SERIAL PRIMARY KEY,
+    reservation_id INTEGER REFERENCES reservation(id),
+    amount FLOAT,
+    type VARCHAR(30)
+);
+INSERT INTO reservation (vehicle_id, customer_id, start_date, end_date) VALUES ('71adg7632b', 1, '10-22-21', '11-22-21'),
+                                                                               ('23bb385ned', 2, '11-15-22', '11-25-22'),
+                                                                               ('23bb385ned', 3, '11-20-22', '11-23-22'),
+                                                                               ('23bb385ned', 1, '12-01-22', '12-03-22'),
+                                                                               ('2g34b73bff', 1, '05-09-19', '06-09-19');
+
+INSERT INTO transaction (reservation_id, amount, type) VALUES (1, 99999999.99, 'Charge'),
+                                                              (1, -550.50, 'Refund'),
+                                                              (2, 15000, 'Charge'),
+                                                              (3, 1999.99, 'Charge'),
+                                                              (3, -10.50, 'Refund'),
+                                                              (3, 20.30, 'Charge');
